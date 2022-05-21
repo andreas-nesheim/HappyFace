4980eebbfc8b63b2b4ffdf96c7ce75a772a74a1e
Andreas Aronsen Nesheim <andreas.nesheim@capgemini.com>
2022-05-21 13:50:09 +0200

    get image from stream

--- HappyFace/MainPage.xaml.cs
+++ HappyFace/MainPage.xaml.cs
@@ -5,23 +5,32 @@ namespace HappyFace;
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-	private async void OnCounterClicked(object sender, EventArgs e)
-	{
-		//var photo = await MediaPicker.PickPhotoAsync();
+    private async void OnCounterClicked(object sender, EventArgs e)
+    {
+        var photo = await MediaPicker.PickPhotoAsync();
+        //var photo = await MediaPicker.CapturePhotoAsync();
 
-		var credentials = new ApiKeyServiceClientCredentials(Constants.SubscriptionKey);
+        var credentials = new ApiKeyServiceClientCredentials(Constants.SubscriptionKey);
         var faceClient = new FaceClient(credentials) { Endpoint = Constants.Endpoint };
-		var url = "https://csdx.blob.core.windows.net/resources/Face/Images/Family1-Dad2.jpg";
-		var faceAttributes = new FaceAttributeType[] { FaceAttributeType.Smile };
-        var detectResult = await faceClient.Face.DetectWithUrlAsync(url, returnFaceAttributes: faceAttributes);
-		foreach (var face in detectResult)
+        var url = "https://csdx.blob.core.windows.net/resources/Face/Images/Family1-Dad2.jpg";
+        var faceAttributes = new FaceAttributeType[] { FaceAttributeType.Smile };
+
+        using FileStream fs = File.OpenRead(photo.FullPath);
+        var detectResult = await faceClient.Face.DetectWithStreamAsync(fs, returnFaceAttributes: faceAttributes);
+        foreach (var face in detectResult)
         {
-			var attr = face.FaceAttributes;
+            var attr = face.FaceAttributes;
         }
-	}
+
+        //var detectResult = await faceClient.Face.DetectWithUrlAsync(url, returnFaceAttributes: faceAttributes);
+        //foreach (var face in detectResult)
+        //{
+        //    var attr = face.FaceAttributes;
+        //}
+    }
 }
