7e61715bedfc13042229d5c3e0d1fe6c7cdca62c
Andreas Aronsen Nesheim <andreas.nesheim@capgemini.com>
2022-05-21 13:29:48 +0200

    return smile as face attribute

--- HappyFace/MainPage.xaml.cs
+++ HappyFace/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
 
 namespace HappyFace;
 
@@ -11,16 +12,16 @@ public partial class MainPage : ContentPage
 
 	private async void OnCounterClicked(object sender, EventArgs e)
 	{
-		var photo = await MediaPicker.PickPhotoAsync();
+		//var photo = await MediaPicker.PickPhotoAsync();
 
 		var credentials = new ApiKeyServiceClientCredentials(Constants.SubscriptionKey);
         var faceClient = new FaceClient(credentials) { Endpoint = Constants.Endpoint };
 		var url = "https://csdx.blob.core.windows.net/resources/Face/Images/Family1-Dad2.jpg";
-        var detectResult = await faceClient.Face.DetectWithUrlAsync(url);
+		var faceAttributes = new FaceAttributeType[] { FaceAttributeType.Smile };
+        var detectResult = await faceClient.Face.DetectWithUrlAsync(url, returnFaceAttributes: faceAttributes);
 		foreach (var face in detectResult)
         {
 			var attr = face.FaceAttributes;
         }
 	}
 }
-
